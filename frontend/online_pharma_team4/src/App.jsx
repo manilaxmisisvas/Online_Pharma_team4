2bb247321192ad505b75a3e885453f5f1726a6e8
K NANDINI <kooranandini@gmail.com>
2025-07-28 21:59:14 +0530

    Resolved merge conflict in App.jsx

--- frontend/online_pharma_team4/src/App.jsx
+++ frontend/online_pharma_team4/src/App.jsx
@@ -1,14 +1,18 @@
+import { useState } from 'react';
+import './App.css';
+
 import UserDashboard from "./components/UserDashboard";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./components/Login.jsx";
 import Admin from "./components/Admin.jsx";
 import Register from "./components/Register.jsx";
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Register />} /> 
-        <Route path="/login" element={<Login/>} />
+        <Route path="/" element={<Register />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/user" element={<UserDashboard />} />
         <Route path="/admin" element={<Admin />} />
       </Routes>
